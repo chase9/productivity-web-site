6bd5b2e9d9dde050c8ccc7570226c224bdde48c5
Chase Lau <chase9@mac.com>
2018-02-28 09:42:49 -0600

    Added photo viewer and 20 photos

---
+++ scripts.js
@@ -0,0 +1,94 @@
+// Lightbox Scripts
+var lightbox = document.getElementById('lightbox');
+var close = document.getElementById('close');
+var images = document.getElementsByClassName('flex-img');
+var lightboxImg = document.getElementById('lightbox-image');
+var lightboxThumbBox = document.getElementById('lightbox-thumbs');
+var playButton = document.getElementById('play');
+
+var slideshow = false;
+
+for (var i = 0; i < images.length; i++) {
+    images[i].onclick = function () {
+        lightbox.style.visibility = "visible";
+        lightbox.style.opacity = '1';
+        lightboxImg.src = this.src;
+    };
+
+    lightboxThumbBox.innerHTML += "<img class='lightbox-thumb' src='" + images[i].src + "'>";
+
+    var lightBoxThumbs = document.getElementsByClassName('lightbox-thumb');
+    for (var i = 0; i < lightBoxThumbs.length; i++) {
+        lightBoxThumbs[i].onclick = function () {
+            lightbox.style.visibility = "visible";
+            lightbox.style.opacity = '1';
+            lightboxImg.src = this.src;
+        };
+    }
+}
+
+// Hide lightbox on 'esc' or pressing close icon.
+document.addEventListener("keypress", function (ev) {
+    if (ev.keyCode === 27) {
+        lightbox.style.visibility = "hidden";
+        lightbox.style.opacity = '0';
+    } else if (ev.keyCode === 37) {
+        previous();
+    } else if (ev.keyCode === 39) {
+        next();
+    }
+});
+
+close.onclick = function () {
+    lightbox.style.visibility = "hidden";
+    lightbox.style.opacity = '0';
+};
+
+function getCurrentIndex() {
+    for (var i = 0; i < images.length; i++) {
+        if (images[i].src === lightboxImg.src) {
+            return i;
+        }
+    }
+}
+
+function previous() {
+    var currIndex = getCurrentIndex();
+    if (currIndex === 0) {
+        lightboxImg.src = images[images.length - 1].src;
+    } else {
+        lightboxImg.src = images[getCurrentIndex() - 1].src;
+    }
+}
+
+function next() {
+    var currIndex = getCurrentIndex();
+    if (currIndex === images.length - 1) {
+        lightboxImg.src = images[0].src;
+    } else {
+        lightboxImg.src = images[currIndex + 1].src;
+    }
+}
+
+var interval;
+
+function toggleSlideshow() {
+    if (playButton.innerText === "Play") {
+        playButton.innerText = 'Pause';
+        slideshow = true;
+    } else {
+        playButton.innerText = 'Play';
+        slideshow = false
+    }
+    console.log(slideshow);
+    if (slideshow) {
+        playSlideshow()
+    } else {
+        clearTimeout(interval)
+    }
+}
+
+function playSlideshow() {
+    next();
+    interval = setTimeout(playSlideshow, 3000);
+}
